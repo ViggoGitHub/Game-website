0615eabf2e38f28ddf05598e0c5987462437294f
ViggoGitHub <viggoks04@gmail.com>
2023-12-20 11:59:06 +0100

    added style to tron

--- spelhemsida/tron.js
+++ spelhemsida/tron.js
@@ -5,33 +5,51 @@ document.addEventListener("DOMContentLoaded", function (){
     let playerY = 0;
     let playerDirection = "right";
 
+
     function updatePlayer() {
         player.style.left = playerX + "px";
         player.style.top = playerY + "px";
     } 
 
-    function movePlayer() {
-        switch(playerDirection){
-            case "right":
-                playerX += 5;
-                break;
-            case "left":
-                playerX -= 5;
-                break;
-        }
+    // function movePlayer() {
+    //     switch(playerDirection){
+    //         case "right":
+    //             playerX += 5;
+    //             break;
+    //         case "left":
+    //             playerX -= 5;
+    //             break;
+    //         case "left":
+    //             playerX -= 5;
+    //             break;
+    //         case "left":
+    //             playerX -= 5;
+    //             break;
+    //     }
 
-        updatePlayer();
-    }
+    //     updatePlayer();
+    // }
 
-    function pressedKey(event){
-        switch(event.key){
-            case "ArrorRight":
-                playerDirection = "right";
-                break;
-            case "ArrorLeft":
-                playerDirection = "left";
-                break;
-        }
+    // function pressedKey(event){
+
+    //     if (KeyboardEvent.w){
+    //         playerY += 5;
+    //     }
+
+        // switch(event.key){
+        //     case "ArrorRight":
+        //         playerDirection = "right";
+        //         break;
+        //     case "ArrorLeft":
+        //         playerDirection = "left";
+        //         break;
+        //     case "ArrorUp":
+        //         playerDirection = "up";
+        //         break;
+        //     case "ArrorDown":
+        //         playerDirection = "down";
+        //         break;
+        // }
     }
 
     // Event listener for keyboard input
