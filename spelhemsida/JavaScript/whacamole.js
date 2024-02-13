2864b45d2a502c5cff197cb96c5a0a088d9b1726
ViggoGitHub <viggoks04@gmail.com>
2024-02-13 14:09:31 +0100

    uppdated the feature to press down the square with the mole

--- spelhemsida/JavaScript/whacamole.js
+++ spelhemsida/JavaScript/whacamole.js
@@ -5,7 +5,6 @@ const score = document.querySelector("#score");
 
 let result = 0;
 
-
 function randomSquare(){
     squares.forEach(square => {
         // removes the mole class if it exist in the square
@@ -14,7 +13,18 @@ function randomSquare(){
     // sets the mole to a random square
     let randomPosition = squares[Math.floor(Math.random() * 9)];
     randomPosition.classList.add("mole");
+
+    hitPosition = randomPosition.id;
 }
+
+squares.forEach(square => {
+    square.addEventListener("mousedown", () => {
+        if (square.id == hitPosition) {
+            result++;
+            score.innerHTML = result;
+        }
+    })
+})
 
 // function that changes the position of the mole after a specific interval
 function moveMole(){
@@ -22,15 +32,10 @@ function moveMole(){
     timerId = setInterval(randomSquare, 500)
 }
 
-for (let i = 0; i < squares.length; i++){
-    squares[i].onclick = () => {
-        if (squares[i].classList.contains("mole")){
-            result += 1;
-            score.innerHTML = result;
-        }
     }
 }
 
+// makes the countDowns function execute every 1000 millisecond
 function update() {
     location.reload();
 }
